c7c6405c027546fe657887c609565f02ff192257
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-14 20:18:12 -0400

    removed unnecessary Usings - end of Section 4

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +8,7 @@ using Persistence;
 // namespace DatingApp.API.Controllers
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+  [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
