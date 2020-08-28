6dc530cd2819ef1a58f35f424417145d6b54ef55
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-27 22:55:39 -0400

    implemented MobX observable map

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,21 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed } from 'mobx';
 import { createContext } from 'react';
 import { IActivity } from '../models/activity';
 import agent from '../api/agent';
 
 class ActivityStore {
+  @observable activityRegistry = new Map();
   @observable activities: IActivity[] = [];
   @observable selectedActivity: IActivity | undefined;
   @observable loadingInitial = false;
   @observable editMode = false;
+  @observable submitting = false;
+
+  @computed get activitiesByDate() {
+    return Array.from(this.activityRegistry.values()).sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
 
   @action loadActivities = async () => {
     this.loadingInitial = true;
@@ -15,7 +23,7 @@ class ActivityStore {
       const activities = await agent.Activities.list();
       activities.forEach((activity) => {
         activity.date = activity.date.split('.')[0];
-        this.activities.push(activity);
+        this.activityRegistry.set(activity.id, activity);
       });
       this.loadingInitial = false;
     } catch (error) {
@@ -24,8 +32,26 @@ class ActivityStore {
     }
   }
 
+  @action createActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.create(activity);
+      this.activityRegistry.set(activity.id, activity);
+      this.editMode = false;
+      this.submitting = false;
+    } catch (error) {
+      this.submitting = false;
+      console.log(error);
+    }
+  };
+
+  @action openCreateForm = () => {
+    this.editMode = true;
+    this.selectedActivity = undefined;
+  }
+
   @action selectActivity = (id: string) => {
-    this.selectedActivity = this.activities.find(a => a.id === id);
+    this.selectedActivity = this.activityRegistry.get(id);
     this.editMode = false;
   }
 }
