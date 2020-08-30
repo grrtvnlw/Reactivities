8629238b713aeddfc73ace3807b188003c3124b1
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-30 16:49:47 -0400

    Cleaning up code

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,8 @@
 import { observable, action, computed } from 'mobx';
-import { createContext } from 'react';
+import { createContext, SyntheticEvent } from 'react';
 import { IActivity } from '../models/activity';
 import agent from '../api/agent';
+import { timeStamp } from 'console';
 
 class ActivityStore {
   @observable activityRegistry = new Map();
@@ -10,6 +11,7 @@ class ActivityStore {
   @observable loadingInitial = false;
   @observable editMode = false;
   @observable submitting = false;
+  @observable target = '';
 
   @computed get activitiesByDate() {
     return Array.from(this.activityRegistry.values()).sort(
@@ -45,9 +47,51 @@ class ActivityStore {
     }
   };
 
+  @action editActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.update(activity);
+      this.activityRegistry.set(activity.id, activity);
+      this.selectedActivity = activity;
+      this.editMode = false;
+      this.submitting = false;
+    } catch (error) {
+      this.submitting = false;
+      console.log(error);
+    }
+  }
+
+  @action deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+    this.submitting = true;
+    this.target = event.currentTarget.name;
+    try {
+      await agent.Activities.delete(id);
+      this.activityRegistry.delete(id);
+      this.submitting = false;
+      this.target = '';
+    } catch (error) {
+      this.submitting = false;
+      this.target = '';
+      console.log(error);
+    } 
+  }
+
   @action openCreateForm = () => {
     this.editMode = true;
     this.selectedActivity = undefined;
+  }
+
+  @action openEditForm = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = true;
+  }
+
+  @action cancelSelectedActivity = () => {
+    this.selectedActivity = undefined;
+  }
+
+  @action cancelFormOpen = () => {
+    this.editMode = false;
   }
 
   @action selectActivity = (id: string) => {
