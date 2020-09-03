713e36e91943a08425b8d3b2646ef7f615f5acb1
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-09-02 23:11:39 -0400

    editing home page

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ configure({enforceActions: 'always'});
 class ActivityStore {
   @observable activityRegistry = new Map();
   @observable activities: IActivity[] = [];
-  @observable activity: IActivity | undefined;
+  @observable activity: IActivity | null = null;
   @observable loadingInitial = false;
   @observable editMode = false;
   @observable submitting = false;
@@ -37,7 +37,7 @@ class ActivityStore {
       })
       console.log(error);
     }
-  };
+  }
 
   @action loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
@@ -58,7 +58,11 @@ class ActivityStore {
         console.log(error);
       }
     }
-  };
+  }
+
+  @action clearActivity = () => {
+    this.activity = null;
+  }
 
   getActivity = (id: string) => {
     return this.activityRegistry.get(id);
@@ -79,7 +83,7 @@ class ActivityStore {
       })
       console.log(error);
     }
-  };
+  }
 
   @action editActivity = async (activity: IActivity) => {
     this.submitting = true;
@@ -120,7 +124,7 @@ class ActivityStore {
 
   @action openCreateForm = () => {
     this.editMode = true;
-    this.activity = undefined;
+    this.activity = null;
   }
 
   @action openEditForm = (id: string) => {
@@ -129,7 +133,7 @@ class ActivityStore {
   }
 
   @action cancelSelectedActivity = () => {
-    this.activity = undefined;
+    this.activity = null;
   }
 
   @action cancelFormOpen = () => {
