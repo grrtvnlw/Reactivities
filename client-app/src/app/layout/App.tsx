ac7eec13b6baa3389a7a4557790aaad6d20838f1
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-09-07 18:46:22 -0400

    Cleaned up code - end of Section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,15 @@
-import React, { useEffect, Fragment, useContext } from "react";
+import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
 import { Route, withRouter, RouteComponentProps } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import "semantic-ui-css/semantic.min.css";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
-  const activityStore = useContext(ActivityStore);
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading activities..." />;
-
   return (
     <Fragment>
       <Route exact path="/" component={HomePage} />
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -7,10 +7,8 @@ configure({enforceActions: 'always'});
 
 class ActivityStore {
   @observable activityRegistry = new Map();
-  @observable activities: IActivity[] = [];
   @observable activity: IActivity | null = null;
   @observable loadingInitial = false;
-  @observable editMode = false;
   @observable submitting = false;
   @observable target = '';
 
@@ -18,7 +16,7 @@ class ActivityStore {
     return Array.from(this.activityRegistry.values()).sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
-  }
+  };
 
   @action loadActivities = async () => {
     this.loadingInitial = true;
@@ -37,7 +35,7 @@ class ActivityStore {
       })
       console.log(error);
     }
-  }
+  };
 
   @action loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
@@ -58,15 +56,15 @@ class ActivityStore {
         console.log(error);
       }
     }
-  }
+  };
 
   @action clearActivity = () => {
     this.activity = null;
-  }
+  };
 
   getActivity = (id: string) => {
     return this.activityRegistry.get(id);
-  }
+  };
 
   @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
@@ -74,7 +72,6 @@ class ActivityStore {
       await agent.Activities.create(activity);
       runInAction('creating activity', () => {
         this.activityRegistry.set(activity.id, activity);
-        this.editMode = false;
         this.submitting = false;
       })
     } catch (error) {
@@ -83,7 +80,7 @@ class ActivityStore {
       })
       console.log(error);
     }
-  }
+  };
 
   @action editActivity = async (activity: IActivity) => {
     this.submitting = true;
@@ -92,7 +89,6 @@ class ActivityStore {
       runInAction('editing activity', () => {
         this.activityRegistry.set(activity.id, activity);
         this.activity = activity;
-        this.editMode = false;
         this.submitting = false;
       })
     } catch (error) {
@@ -101,7 +97,7 @@ class ActivityStore {
       })
       console.log(error);
     }
-  }
+  };
 
   @action deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
     this.submitting = true;
@@ -120,30 +116,7 @@ class ActivityStore {
       })
       console.log(error);
     } 
-  }
-
-  @action openCreateForm = () => {
-    this.editMode = true;
-    this.activity = null;
-  }
-
-  @action openEditForm = (id: string) => {
-    this.activity = this.activityRegistry.get(id);
-    this.editMode = true;
-  }
-
-  @action cancelSelectedActivity = () => {
-    this.activity = null;
-  }
-
-  @action cancelFormOpen = () => {
-    this.editMode = false;
-  }
-
-  @action selectActivity = (id: string) => {
-    this.activity = this.activityRegistry.get(id);
-    this.editMode = false;
-  }
+  };
 }
 
 export default createContext(new ActivityStore());
