4aff3eeeff210cd4f2db553905affa8c376e5c5e
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-10-26 15:09:53 -0400

    Added helper function for date/time input

--- client-app/src/app/common/form/DateInput.tsx
+++ client-app/src/app/common/form/DateInput.tsx
@@ -3,8 +3,7 @@ import { FieldRenderProps } from "react-final-form";
 import { Form, FormFieldProps, Label } from "semantic-ui-react";
 import { DateTimePicker } from "react-widgets";
 
-interface IProps
-  extends FieldRenderProps<Date>, FormFieldProps {}
+interface IProps extends FieldRenderProps<Date>, FormFieldProps {}
 
 const DateInput: React.FC<IProps> = ({
   input,
@@ -18,19 +17,21 @@ const DateInput: React.FC<IProps> = ({
 }) => {
   return (
     <Form.Field error={touched && !!error} width={width}>
-    <DateTimePicker 
-      placeholder={placeholder}
-      value={input.value || null}
-      onChange={input.onChange}
-      {...rest}
-    />
-    {touched && error && (
-      <Label basic color="red">
-        {error}
-      </Label>
-    )}
-  </Form.Field>
-  )
-}
+      <DateTimePicker
+        placeholder={placeholder}
+        value={input.value || null}
+        onChange={input.onChange}
+        date={date}
+        time={time}
+        {...rest}
+      />
+      {touched && error && (
+        <Label basic color="red">
+          {error}
+        </Label>
+      )}
+    </Form.Field>
+  );
+};
 
-export default DateInput
+export default DateInput;
--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import { Segment, Grid, Icon } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
-const ActivityDetailedInfo: React.FC<{activity: IActivity}> = ({activity}) => {
+const ActivityDetailedInfo: React.FC<{ activity: IActivity }> = ({
+  activity,
+}) => {
   return (
     <Segment.Group>
       <Segment attached="top">
@@ -22,7 +24,10 @@ const ActivityDetailedInfo: React.FC<{activity: IActivity}> = ({activity}) => {
             <Icon name="calendar" size="large" color="teal" />
           </Grid.Column>
           <Grid.Column width={15}>
-            <span>{format(activity.date!, 'eeee do MMMM')} at {format(activity.date!, 'h:mm a')}</span>
+            <span>
+              {format(activity.date, "eeee do MMMM")} at{" "}
+              {format(activity.date, "h:mm a")}
+            </span>
           </Grid.Column>
         </Grid>
       </Segment>
