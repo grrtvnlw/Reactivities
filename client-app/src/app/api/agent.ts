d521a58632927b8ab1381430ded6055fc6c240c9
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-12-22 17:19:49 -0500

    Client-side login and register

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { IActivity } from '../models/activity';
+import { IUser, IUserFormValues } from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
@@ -19,7 +20,7 @@ axios.interceptors.response.use(undefined, error => {
   if (status === 500) {
     toast.error('Server error - check the terminal for more info!');
   }
-  throw error;
+  throw error.response;
 })
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -42,6 +43,13 @@ const Activities = {
   delete: (id: string) => requests.del(`/activities/${id}`)
 }
 
+const User = {
+  current: (): Promise<IUser> => requests.get('/user'),
+  login: (user: IUserFormValues): Promise<IUser> => requests.post('/user/login', user),
+  register: (user: IUserFormValues): Promise<IUser> => requests.post('/user/register', user),
+}
+
 export default {
-  Activities
+  Activities,
+  User
 }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,13 +3,19 @@ import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
-import { Route, withRouter, RouteComponentProps, Switch } from "react-router-dom";
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch,
+} from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import "semantic-ui-css/semantic.min.css";
 import NotFound from "./NotFound";
 import { ToastContainer } from "react-toastify";
+import LoginForm from "../../features/user/LoginForm";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
@@ -19,7 +25,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
         path={"/(.+)"}
         render={() => (
           <Fragment>
-            <ToastContainer position='bottom-right' />
+            <ToastContainer position="bottom-right" />
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
               <Switch>
@@ -30,6 +36,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
+                <Route path="/login" component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
