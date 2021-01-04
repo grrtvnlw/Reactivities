cf5107cec90e6853a8121b984ec4bc02b07d60ea
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-01-04 11:41:31 -0500

    End of Section 18

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Tab } from "semantic-ui-react";
+import ProfileDescription from "./ProfileDescription";
 import ProfilePhotos from "./ProfilePhotos";
 
 const panes = [
-  { menuItem: "About", render: () => <Tab.Pane>About content</Tab.Pane> },
+  { menuItem: "About", render: () => <ProfileDescription /> },
   {
     menuItem: "Photos",
     render: () => <ProfilePhotos />,
@@ -28,7 +29,6 @@ const ProfileContent = () => {
       menu={{ fluid: true, vertical: true }}
       menuPosition="right"
       panes={panes}
-      activeIndex={1}
     />
   );
 };
